8d868268e1e093361da70b750628a1d0f58cafa9
John J <7483411+jjakkc@users.noreply.github.com>
2019-07-01 00:02:26 -0400

    Deploying github page

---
+++ precache-manifest.f74bea887f18f3f8707d51cfc1536870.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotronic-friends-app/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "bd3402568da5a1fd966b",
+    "url": "/robotronic-friends-app/static/js/runtime~main.de000560.js"
+  },
+  {
+    "revision": "df5f2569c43cf48fd91f",
+    "url": "/robotronic-friends-app/static/js/main.3bf6caa0.chunk.js"
+  },
+  {
+    "revision": "3d2399b1e13989e9cb9a",
+    "url": "/robotronic-friends-app/static/js/2.d2123a26.chunk.js"
+  },
+  {
+    "revision": "df5f2569c43cf48fd91f",
+    "url": "/robotronic-friends-app/static/css/main.ac5df7cd.chunk.css"
+  },
+  {
+    "revision": "3d2399b1e13989e9cb9a",
+    "url": "/robotronic-friends-app/static/css/2.15634dc2.chunk.css"
+  },
+  {
+    "revision": "f972d48f5edf6e40069333578c395640",
+    "url": "/robotronic-friends-app/index.html"
+  }
+];
